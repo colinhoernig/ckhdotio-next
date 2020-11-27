3c8fdbd1ee7f6735e99f553c9fd209f9b87bda8c
Colin Hoernig <hoernig.colin@gmail.com>
2020-11-27 12:59:49 -0500

    refactor: clean up SocialIconRow

--- src/components/SocialIconRow.tsx
+++ src/components/SocialIconRow.tsx
@@ -1,15 +1,39 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+const SOCIAL_LINKS = {
+  github: {
+    service: "GitHub",
+    url: "https://github.com/colinhoernig",
+    handle: "@colinhoernig",
+    srPretext: "View",
+    icon: FaGithub,
+  },
+  twitter: {
+    service: "Twitter",
+    url: "https://twitter.com/colinhoernig",
+    handle: "@colinhoernig",
+    srPretext: "Follow",
+    icon: FaTwitter,
+  },
+  linkedin: {
+    service: "LinkedIn",
+    url: "https://linkedin.com/in/colinhoernig",
+    handle: "Colin Hoernig",
+    srPretext: "Connect with",
+    icon: FaLinkedin,
+  },
+};
 
 const SocialIconRow = () => (
   <div className="flex justify-center align-center space-x-4">
-    <a href="https://github.com/colinhoernig">
-      <span className="sr-only">Follow @colinhoernig on GitHub</span>
-      <FaGithub className="h-8 w-8" />
-    </a>
-    <a href="https://linkedin.com/in/colinhoernig">
-      <span className="sr-only">Connect with colinhoernig on LinkedIn</span>
-      <FaLinkedin className="h-8 w-8" />
-    </a>
+    {Object.values(SOCIAL_LINKS).map((social) => (
+      <a href={social.url} target="_blank">
+        <span className="sr-only">
+          {social.srPretext} {social.handle} on {social.service}
+        </span>
+        {social.icon({ className: "h-8 w-8" })}
+      </a>
+    ))}
   </div>
 );
 
