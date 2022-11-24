0de81973ebd2b68c29943b1240a6e3b6b4823ebb
Colin Hoernig <me@ckh.io>
2022-11-24 14:52:12 -0700

    fix: fix purgecss content paths

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,8 @@ module.exports = {
     plugins: ['tailwindcss', process.env.NODE_ENV === 'production' ?
       [ '@fullhuman/postcss-purgecss', {
           content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}',
+            './src/pages/**/*.{js,jsx,ts,tsx}',
+            './src/components/**/*.{js,jsx,ts,tsx}',
           ],
           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
         },
